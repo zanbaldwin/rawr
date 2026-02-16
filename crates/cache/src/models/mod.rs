5fa3564910ea6397a0f339000dba60b3e0669c2d
Zan Baldwin <hello@zanbaldwin.com>
2026-02-16 16:57:42 +0100

    Crate: Cache (Join Rows)

--- crates/cache/src/models/mod.rs
+++ crates/cache/src/models/mod.rs
@@ -1,6 +1,9 @@
 mod facet;
 mod file;
+mod join;
 mod version;
 
 pub(crate) use self::file::FileRow;
+pub(crate) use self::join::FullJoinRow;
+pub(crate) use self::join::LeftJoinRow;
 pub(crate) use self::version::VersionRow;

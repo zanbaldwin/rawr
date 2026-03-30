fe28160d45c9376d38e4edfb616219500bb0ce57
Zan Baldwin <hello@zanbaldwin.com>
2026-03-30 19:22:33 +0200

    CLI: Export (PDF)

--- src/command/export/epub.rs
+++ src/command/export/epub.rs
@@ -1,11 +1,11 @@
 use super::File;
-use crate::context::AppContext;
+use super::context::ExportContext;
 use crate::error::Result;
 use crate::output::{Line, Loudness, PALETTE, Piece, Pipe};
 use rawr_extract::models::Version;
 use std::process::ExitCode;
 
-pub(crate) async fn export(ctx: &AppContext, _works: Vec<(Version, Vec<File>)>) -> Result<ExitCode> {
+pub(crate) async fn export(ctx: ExportContext<'_>, _works: Vec<(Version, Vec<File>)>) -> Result<ExitCode> {
     let line = Line::new([Piece::fixed("EPUB export is not yet implemented", &PALETTE.warning)]);
     ctx.output.print(Pipe::Err, &line.with_volume(Loudness::Shout));
     Ok(ExitCode::FAILURE)
